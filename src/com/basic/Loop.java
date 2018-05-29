2a8e98d8bae5db1e065bf2553dda81f41bf833cd
srinivasan76 <srinieee076@gmail.com>
2018-05-29 16:32:58 +0530

    loop in java

---
+++ src/com/basic/Loop.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class Loop {
+	public static void main(String[] args){
+		int i=1;
+		while(i<10){
+			System.out.println(i);
+			i++;
+		}
+		for(i=10;i>=1;i--)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
