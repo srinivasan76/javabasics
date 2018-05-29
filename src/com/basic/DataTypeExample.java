3fec58b297b567a55821d11ef600e7323cbc56d1
srinivasan76 <srinieee076@gmail.com>
2018-05-29 15:38:30 +0530

    string concatenate

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String[] args){
+		String firstName="srini";
+		String lastName="EEE";
+		System.out.print(firstName +" "+lastName +" ");
+	}
+
+}
