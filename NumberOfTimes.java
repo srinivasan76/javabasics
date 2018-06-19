23ec34092d265d0c9af13d8985e64a906599b1c7
srinivasan76 <37662901+srinivasan76@users.noreply.github.com>
2018-06-19 16:08:00 +0530

    Add files via upload

---
+++ NumberOfTimes.java
@@ -0,0 +1,14 @@
+package com.guvi;
+
+import java.util.Scanner;
+
+public class NumberOfTimes {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i;
+		for(i=0;i<n;i++) {
+			System.out.println("Hello");
+		}
+}
+}
