8c3ed2211cb60c7ae568c8889c3c0b2fdc68a683
srinivasan76 <37662901+srinivasan76@users.noreply.github.com>
2018-06-19 16:23:26 +0530

    Add files via upload

---
+++ CountInt.java
@@ -0,0 +1,13 @@
+package com.guvi;
+
+import java.util.Scanner;
+
+public class CountInt {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String str=""+n;
+		System.out.println(str.length());
+		
+}
+}
