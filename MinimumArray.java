74cc511e8860a14d19e7529f6938e7c02c8c3281
srinivasan76 <37662901+srinivasan76@users.noreply.github.com>
2018-06-20 15:28:22 +0530

    Add files via upload

---
+++ MinimumArray.java
@@ -0,0 +1,18 @@
+package com.guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinimumArray {
+	public static void main(String[] args) {
+	
+	Scanner sc=new Scanner(System.in);
+	int size=sc.nextInt();
+	int i,arr[]=new int[size];
+	for(i=0;i<size;i++) {
+		arr[i]=sc.nextInt();
+	}
+	Arrays.sort(arr);
+	System.out.println(arr[0]);
+}
+}
