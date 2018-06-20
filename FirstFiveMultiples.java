1071b2f8d0fb332560ad20e86dfdfe2b2b3c10f2
srinivasan76 <37662901+srinivasan76@users.noreply.github.com>
2018-06-20 14:29:42 +0530

    Add files via upload

---
+++ FirstFiveMultiples.java
@@ -0,0 +1,15 @@
+package com.guvi;
+
+import java.util.Scanner;
+
+public class FirstFiveMultiples {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int i;
+		for(i=1;i<=n;i++) {
+			System.out.print(i*5+" ");
+		}
+	}
+
+}
