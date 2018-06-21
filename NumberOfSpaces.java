9a76582e09e843158efec214fabbdfc343b2e209
srinivasan76 <37662901+srinivasan76@users.noreply.github.com>
2018-06-21 18:19:26 +0530

    Add files via upload

---
+++ NumberOfSpaces.java
@@ -0,0 +1,12 @@
+package com.guvi;
+
+import java.util.Scanner;
+
+public class NumberOfSpaces {
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		String input=scan.nextLine();
+		if(input.length()>=1 && input.length()<=1000)
+		System.out.println(input.split(" ").length-1);
+	}
+}
