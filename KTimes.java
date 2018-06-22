285bdf6e98c4b1940ee8e0ba8d46f99440733d6e
srinivasan76 <37662901+srinivasan76@users.noreply.github.com>
2018-06-22 15:40:54 +0530

    Add files via upload

---
+++ KTimes.java
@@ -0,0 +1,14 @@
+package com.guvi;
+
+import java.util.Scanner;
+
+public class KTimes {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	String s=scan.nextLine();
+	int k=scan.nextInt();
+	int i;
+	for(i=0;i<k;i++)
+		System.out.println(s);
+}
+}
